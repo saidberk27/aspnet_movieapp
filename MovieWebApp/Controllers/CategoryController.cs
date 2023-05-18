191049002b05f706f064fa15f840d89719282637
Said Berk <csaidberk@gmail.com>
2023-05-18 14:18:44 +0300

    Update opeartions are done

--- MovieWebApp/Controllers/CategoryController.cs
+++ MovieWebApp/Controllers/CategoryController.cs
@@ -42,5 +42,43 @@ namespace MovieWebApp.Controllers
       
             return View(obj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryA = _db.Categories.FirstOrDefault(u => u.Id ==id); How to use it without enity framework
+            //var categoryB = _db.Categories.SingleOrDefault(u => u.Id == id); How to use it without enity framework
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }    
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Hata", "İki Kısım Aynı Değerde Olamaz");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
     }
 }
--- MovieWebApp/Models/Category.cs
+++ MovieWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieWebApp.Models
 {
@@ -9,6 +10,8 @@ namespace MovieWebApp.Models
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Görüntülenme Sırası")]
+        [Range(1, 100, ErrorMessage = "Çok uzun!")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
